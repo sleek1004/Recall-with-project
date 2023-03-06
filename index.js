8ecedcf7cd3b9b52a381b0820825bb4f23e3c36a
sleek1004 <arojojoyeo@yahoo.com>
2023-03-06 23:48:55 +0100

    simple calculator

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 let figureEL = document.getElementById("figure-el")
-let previewEL = document.getElementById("preview-el")
+let previewEL = document.getElementById ("preview-el")
 
 let count = 0
 
-
 function add(){
-    count += 1
-    figureEL.innerText = count
+     count += 1
+     figureEL.textContent = count
+     
+
 }
 
-function subtract(){
- count = -1
-    figureEL.textContent = count
-}
 
 function save(){
 
 previewEL.textContent += count + "-"
-count = 0
+figureEL.textContent = 0
 }
